6ec89988f2c25f21866f1c580839e32317e17958
Javier Romero <alexandersoriano1022@gmail.com>
2026-01-26 21:22:50 -0600

    Se realizo la funcion de guardar usando la API de localStorage y se valido el codigo sea unico

--- main.js
+++ main.js
@@ -1,14 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
     frmAlumnos.addEventListener("submit", (e) => {
         e.preventDefault();
-
-        let datos = {
-            codigo: txtCodigoAlumno.value,
-            nombre: txtnombreAlumno.value,
-            direccion: txtDireccionAlumno.value,
-            email: txtEmailAlumno.value,
-            telefono: txtTelefonoAlumno.value
-        };
-        console.log(datos);
+       guardarAlumno();
     });
 });
+
+function guardarAlumno() {
+    let datos = {
+        id: getId(),
+        codigo: txtCodigoAlumno.value,
+        nombre: txtnombreAlumno.value,
+        direccion: txtDireccionAlumno.value,
+        email: txtEmailAlumno.value,
+        telefono: txtTelefonoAlumno.value
+    }, codigoDuplicado = buscarAlumno(datos.codigo);
+    if(codigoDuplicado){
+        alert("El codigo " + codigoDuplicado.codigo + " ya existe, "+ codigoDuplicado.nombre);
+        return; 
+    }
+    localStorage.setItem( datos.id, JSON.stringify(datos));
+    limpiarFormulario();
+}
+
+function getId(){
+    return localStorage.length + 1;
+}
+
+function limpiarFormulario(){
+    frmAlumnos.reset();
+}
+
+function buscarAlumno(codigo=''){
+    let n = localStorage.length;
+    for(let i = 0; i < n; i++){
+        let datos = JSON.parse(localStorage.getItem(i));
+        if(datos?.codigo && datos.codigo.trim().toUpperCase() == codigo.trim().toUpperCase()){
+            return datos;
+        }
+    }
+    return null;
+}
