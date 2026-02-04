a7b39e2998c80027c71fe97e1a06cd6f7f4b0288
Javier Romero <alexandersoriano1022@gmail.com>
2026-02-03 20:31:21 -0600

    se termino el CRUD alumno

--- main.js
+++ main.js
@@ -1,74 +1,94 @@
-6+document.addEventListener("DOMContentLoaded", () => {
-    mostrarAlumnos();
+var accion = 'nuevo',
+    id = 0;
+
+document.addEventListener("DOMContentLoaded", () => {
+    txtBuscarAlumno.addEventListener("keyup", (e) => {
+        mostrarAlumnos(e.target.value);
+    });
     frmAlumnos.addEventListener("submit", (e) => {
         e.preventDefault();
        guardarAlumno();
     });
+    frmAlumnos.addEventListener("reset", (e) => {
+        limpiarFormulario();
+    });
+    mostrarAlumnos();
 });
-
-function mostrarAlumnos(){
-    let $tblAlumnos = document.querySelector("#tblAlumnos tbody");
-     n = localStorage.length;
-    filas = "";
+function mostrarAlumnos(buscar=''){
+    let $tblAlumnos = document.querySelector("#tblAlumnos tbody"),
+        n = localStorage.length,
+        filas = "";
     $tblAlumnos.innerHTML = "";
-    
-    for(let i = 0; i < n; i++){
+    for(let i=0; i<n; i++){
         let key = localStorage.key(i);
-        data = JSON.parse(localStorage.getItem(key));
-        if (data != null){
-            filas += `
-            <tr onclick='modificarAlumno(${JSON.stringify(data)})'>
-                <td>${data.codigo}</td>
-                <td>${data.nombre}</td>
-                <td>${data.direccion}</td>
-                <td>${data.email}</td>
-                <td>${data.telefono}</td>
-                <td>
-                    <button class = "btn btn-danger">DEL</button>
-            
-                </td>
-            </tr>`;
+        if( Number(key) ){
+            let data = JSON.parse(localStorage.getItem(key));
+            if( data.nombre.toUpperCase().includes(buscar.toUpperCase()) || 
+                data.codigo.toUpperCase().includes(buscar.toUpperCase()) ){
+                filas += `
+                    <tr onclick='modificarAlumno(${JSON.stringify(data)})'>
+                        <td>${data.codigo}</td>
+                        <td>${data.nombre}</td>
+                        <td>${data.direccion}</td>
+                        <td>${data.email}</td>
+                        <td>${data.telefono}</td>
+                        <td>
+                            <button class="btn btn-danger" onclick='eliminarAlumno(${data.id}, event)'>DEL</button>
+                        </td>
+                    </tr>
+                `;
+            }
         }
     }
-    tblAlumnos.innerHTML = filas;
+    $tblAlumnos.innerHTML = filas;
 }
-
-function modificarAlumno(alumno) {
+function eliminarAlumno(id, e){
+    e.stopPropagation();
+    if(confirm("¿Está seguro de eliminar el alumno?")){
+        localStorage.removeItem(id);
+        mostrarAlumnos();
+    }
+}
+function modificarAlumno(alumno){
+    accion = 'modificar';
+    id = alumno.id;
     txtCodigoAlumno.value = alumno.codigo;
     txtnombreAlumno.value = alumno.nombre;
     txtDireccionAlumno.value = alumno.direccion;
     txtEmailAlumno.value = alumno.email;
     txtTelefonoAlumno.value = alumno.telefono;
-
-
-
 }
-
 function guardarAlumno() {
+    console.log(accion);
     let datos = {
-        id: getId(),
+        id: accion=='modificar' ? id : getId(),
         codigo: txtCodigoAlumno.value,
         nombre: txtnombreAlumno.value,
         direccion: txtDireccionAlumno.value,
         email: txtEmailAlumno.value,
         telefono: txtTelefonoAlumno.value
     }, codigoDuplicado = buscarAlumno(datos.codigo);
-    if(codigoDuplicado){
+    if(codigoDuplicado && accion=='nuevo'){
         alert("El codigo del alumno ya existe, "+ codigoDuplicado.nombre);
-        return;
+        return; //Termina la ejecucion de la funcion
     }
-    localStorage.setItem( datos.id, JSON.stringify(datos)); 
-
-    mostrarAlumnos();
+    localStorage.setItem( datos.id, JSON.stringify(datos));
     limpiarFormulario();
+    mostrarAlumnos();
 }
 
 function getId(){
-    return localStorage.length + 1;
+    return new Date().getTime();
 }
 
 function limpiarFormulario(){
-    frmAlumnos.reset();
+    accion = 'nuevo';
+    id = 0;
+    txtCodigoAlumno.value = '';
+    txtnombreAlumno.value = '';
+    txtDireccionAlumno.value = '';
+    txtEmailAlumno.value = '';
+    txtTelefonoAlumno.value = '';
 }
 
 function buscarAlumno(codigo=''){
