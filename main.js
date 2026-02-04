8838749a74681f2968fd662f842d2abcc164da67
Javier Romero <alexandersoriano1022@gmail.com>
2026-02-03 19:44:26 -0600

    Se agrego dos columnas para mostrar los datos de la tabla

--- main.js
+++ main.js
@@ -1,9 +1,48 @@
-document.addEventListener("DOMContentLoaded", () => {
+6+document.addEventListener("DOMContentLoaded", () => {
+    mostrarAlumnos();
     frmAlumnos.addEventListener("submit", (e) => {
         e.preventDefault();
        guardarAlumno();
     });
 });
+
+function mostrarAlumnos(){
+    let $tblAlumnos = document.querySelector("#tblAlumnos tbody");
+     n = localStorage.length;
+    filas = "";
+    $tblAlumnos.innerHTML = "";
+    
+    for(let i = 0; i < n; i++){
+        let key = localStorage.key(i);
+        data = JSON.parse(localStorage.getItem(key));
+        if (data != null){
+            filas += `
+            <tr onclick='modificarAlumno(${JSON.stringify(data)})'>
+                <td>${data.codigo}</td>
+                <td>${data.nombre}</td>
+                <td>${data.direccion}</td>
+                <td>${data.email}</td>
+                <td>${data.telefono}</td>
+                <td>
+                    <button class = "btn btn-danger">DEL</button>
+            
+                </td>
+            </tr>`;
+        }
+    }
+    tblAlumnos.innerHTML = filas;
+}
+
+function modificarAlumno(alumno) {
+    txtCodigoAlumno.value = alumno.codigo;
+    txtnombreAlumno.value = alumno.nombre;
+    txtDireccionAlumno.value = alumno.direccion;
+    txtEmailAlumno.value = alumno.email;
+    txtTelefonoAlumno.value = alumno.telefono;
+
+
+
+}
 
 function guardarAlumno() {
     let datos = {
@@ -15,10 +54,12 @@ function guardarAlumno() {
         telefono: txtTelefonoAlumno.value
     }, codigoDuplicado = buscarAlumno(datos.codigo);
     if(codigoDuplicado){
-        alert("El codigo " + codigoDuplicado.codigo + " ya existe, "+ codigoDuplicado.nombre);
-        return; 
+        alert("El codigo del alumno ya existe, "+ codigoDuplicado.nombre);
+        return;
     }
-    localStorage.setItem( datos.id, JSON.stringify(datos));
+    localStorage.setItem( datos.id, JSON.stringify(datos)); 
+
+    mostrarAlumnos();
     limpiarFormulario();
 }
 
@@ -33,7 +74,8 @@ function limpiarFormulario(){
 function buscarAlumno(codigo=''){
     let n = localStorage.length;
     for(let i = 0; i < n; i++){
-        let datos = JSON.parse(localStorage.getItem(i));
+        let key = localStorage.key(i);
+        let datos = JSON.parse(localStorage.getItem(key));
         if(datos?.codigo && datos.codigo.trim().toUpperCase() == codigo.trim().toUpperCase()){
             return datos;
         }
